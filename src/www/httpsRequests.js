8f33a405020a7a8b7bbf678e571747f6859cc157
rahulfordev <rahulforsms@gmail.com>
2024-11-07 21:16:52 +0600

    base url of the api has been changed

--- src/www/httpsRequests.js
+++ src/www/httpsRequests.js
@@ -2,10 +2,14 @@ import axios from "axios";
 
 // get attachment count
 export const getAttachmentCount = async (id) =>
-  axios.get(`http://localhost:5000/api/attachments/${id}/count`);
+  axios.get(`https://task-backend-code.vercel.app/api/attachments/${id}/count`);
 
 // post attachment
 export const postAttachment = async (id, formData) =>
-  axios.post(`http://localhost:5000/api/upload/${id}`, formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
+  axios.post(
+    `https://task-backend-code.vercel.app/api/upload/${id}`,
+    formData,
+    {
+      headers: { "Content-Type": "multipart/form-data" },
+    }
+  );
